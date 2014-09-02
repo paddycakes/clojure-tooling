70f76337ce09be272f1042f632b2c18f8a7886aa
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-09-02 21:33:26 +0100

    Adding tools.trace and tools.namespace dependencies

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,10 @@
 {:user {:plugins [[lein-try "0.4.3"]
                   [lein-ancient "0.5.5"]]
-        :dependencies [[im.chit/vinyasa "0.1.8"]
+        :dependencies [[org.clojure/tools.trace "0.7.8"]
+                       [org.clojure/tools.namespace "0.2.5"]
+                       [im.chit/vinyasa "0.1.8"]
                        [org.clojars.gjahad/debug-repl "0.3.3"]]
-        :injections [(require 'vinyasa.inject)
-                     #_(require 'alex-and-georges.debug-repl)
+        :injections [(require 'vinyasa.inject) 
                      (vinyasa.inject/inject 'clojure.core '>
                        '[[clojure.repl doc source]
-                         [clojure.pprint pprint pp]])
-                     #_(vinyasa.inject/inject 'clojure.core '>
-                       '[[alex-and-georges.debug-repl debug-repl]])]}}
+                         [clojure.pprint pprint pp]])]}}
