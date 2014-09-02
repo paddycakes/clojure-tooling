760c2c577a220908eda846af5768c8888b84aa19
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-09-02 18:00:56 +0100

    First commit of lein personal user profile configuration

---
+++ profiles.clj
@@ -0,0 +1,10 @@
+{:user {:plugins [[lein-try "0.4.3"]]
+        :dependencies [[im.chit/vinyasa "0.1.8"]
+                       [org.clojars.gjahad/debug-repl "0.3.3"]]
+        :injections [(require 'vinyasa.inject)
+                     #_(require 'alex-and-georges.debug-repl)
+                     (vinyasa.inject/inject 'clojure.core '>
+                       '[[clojure.repl doc source]
+                         [clojure.pprint pprint pp]])
+                     #_(vinyasa.inject/inject 'clojure.core '>
+                       '[[alex-and-georges.debug-repl debug-repl]])]}}
